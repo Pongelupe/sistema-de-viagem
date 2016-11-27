b98c967e35f4e195dbbceeb1d984c031d615aed1
Luiz Baldi <baldilp@gmail.com>
2016-11-27 20:51:03 -0200

    fixing bugs

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ typedef struct {
 } infoVoo;
 
 // -- Funções para controle de fluxo principal
-char getOpcao() {
+char getOpcaoPrincipal() {
 	char ch;
 
 	printf("\n-- Menu de opcoes --\n");
@@ -30,50 +30,76 @@ char getOpcao() {
 	return ch;
 }
 
+int getOpcaoImprimir() {
+    int opcaoImprimir;
+
+    printf("\n-- Opcoes de impressao --\n");
+    printf("\n1. Imprimir dados de um voo");
+    printf("\n2. Imprimir ocupacao dos voos");
+    printf("\n3. Imprimir todos os dados");
+
+    printf("\n\nEscolha a opcao desejada: ");
+    scanf("%d", &opcaoImprimir);
+    return opcaoImprimir;
+}
+
 void clearBuffer(char c) {
 	while(c != '\n') {
 		scanf("%c", &c);
 	}
 }
 
+int getNumeroVoo() {
+    int numeroVoo;
+
+    printf("\nNumero do voo: ");
+    scanf("%d", &numeroVoo);
+
+    return numeroVoo;
+}
+
 void iniciar() {
-    // Declaração de variáveis necessárias para o funcionamento do sistema
+    // Variáveis de sistema
     infoVoo airBus[50];
     long int idCliente;
     int nVoo;
 
-    // Inicializa variavel para carregar menu
-    char opcao;
+    // Variáveis de menu
+    char opcaoPrincipal;
+    int opcaoImprimir;
 
-	while (opcao != 's') {
-		opcao = getOpcao();
-		clearBuffer(opcao);
+	while (opcaoPrincipal != 's') {
+		opcaoPrincipal = getOpcaoPrincipal();
+		clearBuffer(opcaoPrincipal);
 
-		switch (opcao) {
+		switch (opcaoPrincipal) {
 			case 'a':
 				printf("Inicializando dados...");
                 inicializarDados(airBus);
 				break;
 			case 'b':
-			    printf("Digite somente os numeros da sua identidade:\n");
+			    printf("Digite somente os numeros da sua identidade: ");
 			    scanf("%li", &idCliente);
-			    printf("\nAgora digite o numero do voo:\n");
-			    scanf("%d", &nVoo);
+			    nVoo = getNumeroVoo();
 				printf("Incluindo reserva...");
 				incluirReserva(airBus, idCliente, nVoo);
-				clearBuffer(opcao);
+				clearBuffer(opcaoPrincipal);
 				break;
 			case 'c':
 				printf("Excluindo reserva...");
 				break;
 			case 'd':
+                opcaoImprimir = getOpcaoImprimir();
+                nVoo = getNumeroVoo();
 				printf("Imprimindo reserva...");
+                imprimirReserva(airBus, opcaoImprimir, nVoo);
+                clearBuffer(opcaoPrincipal);
 				break;
             case 's':
 				printf("Encerrando...");
 				break;
 			default:
-			    printf("\n-- Digitado: %c\n", opcao);
+			    printf("\n-- Digitado: %c\n", opcaoPrincipal);
 				printf("A opcao selecionada e invalida, digite novamente");
 				break;
 		}
@@ -85,7 +111,7 @@ int inicializarDados(infoVoo airBus[50]){
     int i, j , n = 1000;
 
     for(i = 0; i < 50; i++) {
-        airBus[i].capacidade = 10;
+        airBus[i].capacidade = 0;
         airBus[i].numeroVoo = n;
         n++;
     }
@@ -94,15 +120,14 @@ int inicializarDados(infoVoo airBus[50]){
 }
 
 //-- Incluir reserva
-int incluirReserva(infoVoo airBus[50],int idCliente, int nVoo) {
+int incluirReserva(infoVoo airBus[50], int idCliente, int nVoo) {
     int i, j;
 
     for (i = 0; i < 50; i++) {
         if (nVoo == airBus[i].numeroVoo) {
             if (airBus[i].capacidade == 10) {
                 return 1;
-            }
-            else {
+            } else {
                 for (j = 0; j < 10; j++) {
                     if (airBus[i].poltrona[j].estadoAssento == 0) {
                         airBus[i].capacidade++;
@@ -112,10 +137,10 @@ int incluirReserva(infoVoo airBus[50],int idCliente, int nVoo) {
                     }
                 }
             }
-             printf("Reserva incluida com sucesso!");
+            printf("\nReserva incluida com sucesso!");
         }
         else{
-            printf("Numero do voo nao encontrado, digite novamente.");
+            printf("\nNumero do voo nao encontrado, digite novamente.");
         }
 
     }
@@ -148,16 +173,16 @@ int excluirReserva(infoVoo airBus[50],int idCliente,int numeroVoo) {
 }
 
 //-- Imprimir reserva
-void imprimirReserva(infoVoo airBus[50],int opcao,int numeroVoo) {
+void imprimirReserva(infoVoo airBus[50], int opcao, int numeroVoo) {
     int i,j;
     int vooInexistente = 1;
     switch (opcao) {
     case 1:
-        printf("Numero do voo: %d\n",numeroVoo);
+        printf("\nNumero do voo: %d\n",numeroVoo);
         for ( i=0; i<50; i++) {
             if (numeroVoo==airBus[i].numeroVoo) {
                 vooInexistente=0;
-                printf("Numero de reservas do voo: %d\n",airBus[i].capacidade);
+                printf("\nNumero de reservas do voo: %d\n",airBus[i].capacidade);
                 for ( j=0; j<10; j++) {
                     if (airBus[i].poltrona[j].estadoAssento==1) {
                         printf("%li\n",airBus[i].poltrona[j].infoPassageiro);
