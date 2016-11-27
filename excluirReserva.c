5ba954d842a3a56b9938330e3b2f46ab06bb0cb3
Pedro <pongelupe1998@gmail.com>
2016-11-27 12:07:16 -0200

    delete function was fixed

--- excluirReserva.c
+++ excluirReserva.c
@@ -13,22 +13,25 @@ typedef struct
     infoAssento poltrona[10];
     int numeroVoo;
 } infoVoo;
-int excluirReserva(infoVoo[50],*idCliente,int numeroVoo)
+int excluirReserva(infoVoo a[50],int idCliente,int numeroVoo)
 {
     int i, j;
-    for(i=0,i<50; i++)
+    for(i=0;i<50; i++)
     {
-        if(numeroVoo==infoVoo[i].numeroVoo)
+        if(numeroVoo==a[i].numeroVoo)
         {
-            for(j=0;j<10;j++)
+            for(j=0; j<10; j++)
             {
-                infoVoo[i].poltrona[j].infoPassageiro=0;
-                infoVoo.capacidade--;
-                infoVoo.poltrona[j].estadoAssento=0;
+                if (idCliente==a[i].poltrona[i].infoPassageiro)
+                {
+                    a[i].poltrona[j].infoPassageiro=0;
+                    a[i].capacidade--;
+                    a[i].poltrona[j].estadoAssento=0;
+                    return 2;
+                }
             }
-            return 2;
+            return 1;
         }
-        return 1;
     }
     return 0;
 }
